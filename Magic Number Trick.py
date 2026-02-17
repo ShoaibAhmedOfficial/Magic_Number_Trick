4720798fa122f577b4becaa16d16114b75e05058
Shoaib Ahmed <161276255+ShoaibAhmedOfficial@users.noreply.github.com>
2026-02-17 09:17:21 +0500

    Add files via upload

---
+++ Magic Number Trick.py
@@ -0,0 +1,8 @@
+print("Think of a number...")
+
+num = int(input("Enter any number: "))
+
+result = (((num * 2) + 10) // 2) - num
+print("First, multiply your given number by 2, then add 10 to it. After that, divide the result by 2, and finally subtract your original number")
+print("Your final result is:", result)
+print("Magic! Result is always 5 😄")
